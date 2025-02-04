8b567d0de11d945a1586380d5945df37fec5e974
Sebastiaan Broekema <sebastiaanbroekema@gmail.com>
2025-02-04 22:08:55 +0100

    wip: fonts now correct

--- module 3/games.py
+++ module 3/games.py
@@ -2,17 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from highlight_text import fig_text, ax_text
+from highlight_text import fig_text
 from pypalettes import load_cmap
-from drawarrow import fig_arrow, ax_arrow
+from drawarrow import fig_arrow
 from pyfonts import load_font
 
 cmap = load_cmap("AsteroidCity3")
 google_font = "https://github.com/google/fonts/blob/main"
-regular = load_font('https://github.com/eifetx/Pixelify-Sans/blob/main/fonts/otf/PixelifySans-Regular.otf?raw=true')
+regular = load_font(
+    "https://github.com/eifetx/Pixelify-Sans/blob/main/fonts/otf/PixelifySans-Regular.otf?raw=true"
+)
 
-bold= load_font('https://github.com/eifetx/Pixelify-Sans/blob/main/fonts/otf/PixelifySans-Bold.otf?raw=true')
-# https://github.com/eifetx/Pixelify-Sans/blob/main/fonts/otf/PixelifySans-Bold.otf
+bold = load_font(
+    "https://github.com/eifetx/Pixelify-Sans/blob/main/fonts/otf/PixelifySans-SemiBold.otf?raw=true"
+)
+
+background = "#f5f6fa"
+
+
 regions = [
     "NA_Sales",
     "EU_Sales",
@@ -36,21 +43,11 @@ percentage = (
 ).reset_index()
 
 
-fig, ax = plt.subplots()
-
-ax.set_xlim(0, 100)
-
-for region, color in zip(regions, cmap):
-    sns.barplot(x=region, y="Genre", data=percentage, color=color, ax=ax)
-
-
-plt.show()
-
 text_props = [{"color": color} for color in cmap.colors]
 
 
 fig, ax = plt.subplots()
-fig.subplots_adjust(top=0.8, right=0.80)
+fig.subplots_adjust(top=0.725, right=0.725, left=0.2)
 ax.set_xlim(0, 100)
 
 percentage.set_index("Genre").plot(
@@ -58,18 +55,13 @@ percentage.set_index("Genre").plot(
 )
 
 
-x_step = [00, 20, 40, 60, 80, 100]
-ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False, length=0)
-ax.set_xticks(x_step, [f"{x}%" for x in x_step])
-
-# Press Start 2P 
 fig_text(
     0.05,
     0.95,
-    "Share of game sales in\n<North America>, <Europa>, <Japan> and <the rest of the world>",
+    "Share of game sales in\n<North America>, <Europa>, <Japan>\nand <the rest of the world>",
     highlight_textprops=text_props,
     font=bold,
-    size=20,
+    size=16,
     ax=ax,
 )
 
@@ -79,20 +71,45 @@ ax.grid(which="major", axis="x", linestyle="--")
 
 disp_arrow = ax.transData.transform((75, 7))
 
+
 # Convert display coordinates to figure coordinates
 fig_coords_arrow = fig.transFigure.inverted().transform(disp_arrow)
 
-fig_arrow((0.81, 0.5), fig_coords_arrow, radius=0.1)
+fig_arrow((0.73, 0.5), fig_coords_arrow, radius=0.1)
 fig_text(
-    0.82,
+    0.73,
     0.5,
     "The only genre where\nNorth America is not\nnumber one in sale share\n<35%> vs <38%>",
-    highlight_textprops=[text_props[0] | {"weight":"bold"}, text_props[2]|{"weight":"bold"}],
+    highlight_textprops=[
+        text_props[0] | {"font": bold},
+        text_props[2] | {"font": bold},
+    ],
     size=10,
-    va= "center",
-    ha="left"
+    va="center",
+    ha="left",
+    font=regular,
 )
 
+x_step = [0, 20, 40, 60, 80, 100]
+ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False, length=0)
+ax.set_xticks(x_step, [f"{x}%" for x in x_step], font=regular)
 
-sns.despine(top=False, bottom=True, left=True, trim=True, offset=10)
+for label in  ax.get_yticklabels():
+    label.set_fontproperties(regular)
+
+print(ax.get_xticklabels())
+
+ax.set_ylabel("")
+
+sns.despine(
+    top=False,
+    bottom=True,
+    left=True,
+    trim=True,
+    # offset=10,
+)
+
+fig.set_facecolor(background)
+ax.set_facecolor(background)
+
 plt.show(block=False)
